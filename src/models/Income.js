bccdd0b32a16f50b55892c6c4943318a59c031fa
vinoth <vinoth27172002@gmail.com>
2026-02-06 23:44:49 +0530

    fix(income): rename deleteIncome to deleteIncomeById and enhance error handling

--- src/models/Income.js
+++ src/models/Income.js
@@ -1,4 +1,5 @@
 import pool from "../db/index.js";
+import AppError from "../utils/AppError.js";
 
 export const createIncome = async ({ userId, icon, source, amount, date, notes }) => {
     const result = await pool.query(
@@ -37,14 +38,22 @@ export const getAllIncomes = async ({ userId }) => {
     return result.rows;
 };
 
-export const deleteIncome = async ({ incomeId, userId }) => {
+export const deleteIncomeById = async ({ incomeId, userId }) => {
     const result = await pool.query(
         `DELETE FROM incomes
         WHERE id = $1
         AND user_id = $2
-        RETURNING
-        id AS incomeId`, [incomeId, userId]
+        RETURNING id AS "incomeId"
+        `, [incomeId, userId]
     );
+    
+    if (result.rowCount === 0) {
+        throw new AppError(
+            "Income not found or not authorized to delete",
+            404,
+            ["Income not found or you are not authorized to delete this income"]
+        )
+    }
 
     return result.rows[0];
 }
