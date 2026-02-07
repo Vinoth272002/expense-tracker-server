40aed0677d4b6e35522721f9b6f97b9f4dc7fd20
vinoth <vinoth27172002@gmail.com>
2026-02-07 16:03:08 +0530

    refactor(income, user): improve query formatting for better readability

--- src/models/Income.js
+++ src/models/Income.js
@@ -2,8 +2,7 @@ import pool from "../db/index.js";
 import AppError from "../utils/AppError.js";
 
 export const createIncome = async ({ userId, icon, source, amount, date, notes }) => {
-    const result = await pool.query(
-        `INSERT INTO incomes
+    const query = `INSERT INTO incomes
         (user_id, icon, source, amount, date, notes)
         VALUES
         ($1, $2, $3, $4, $5, $6)
@@ -15,16 +14,15 @@ export const createIncome = async ({ userId, icon, source, amount, date, notes }
         amount,
         date,
         notes
-        `,
-        [userId, icon, source, amount, date, notes]
-    );
+    `;
+    
+    const result = await pool.query(query, [userId, icon, source, amount, date, notes]);
 
     return result.rows[0];
 };
 
 export const getAllIncomes = async ({ userId }) => {
-    const result = await pool.query(
-        `SELECT
+    const query = `SELECT
         id AS "incomeId",
         user_id AS "userId",
         icon,
@@ -32,20 +30,23 @@ export const getAllIncomes = async ({ userId }) => {
         amount,
         date,
         notes
-        FROM incomes WHERE user_id = $1 AND amount > 0 ORDER BY id ASC`, [userId]
-    );
+        FROM incomes WHERE user_id = $1
+        ORDER BY date DESC
+    `;
+    
+    const result = await pool.query(query, [userId]);
 
     return result.rows;
 };
 
 export const deleteIncomeById = async ({ incomeId, userId }) => {
-    const result = await pool.query(
-        `DELETE FROM incomes
+    const query = `DELETE FROM incomes
         WHERE id = $1
         AND user_id = $2
         RETURNING id AS "incomeId"
-        `, [incomeId, userId]
-    );
+    `;
+    
+    const result = await pool.query(query, [incomeId, userId]);
     
     if (result.rowCount === 0) {
         throw new AppError(
@@ -56,4 +57,29 @@ export const deleteIncomeById = async ({ incomeId, userId }) => {
     }
 
     return result.rows[0];
+};
+
+export const getAllIncomesForExport = async({ userId }) => {
+    const query = `SELECT
+        source,
+        amount,
+        date,
+        notes,
+        created_at AS "createdAt"
+        FROM incomes
+        WHERE user_id = $1
+        ORDER BY date DESC
+    `;
+
+    const result = await pool.query(query, [userId]);
+
+    if (result.rowCount === 0) {
+        throw new AppError(
+            "No incomes records found",
+            404,
+            ["No income records found for export"]
+        )
+    }
+
+    return result.rows;
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,46 +1,47 @@
 import pool from "../db/index.js";
 
 export const createUser = async ({ fullName, email, password, profilePicUrl }) => {
-    const result = await pool.query(
-       `INSERT INTO users  (full_name, email, password, profile_pic_url) VALUES ($1, $2, $3, $4)
+    const query = `INSERT INTO users  (full_name, email, password, profile_pic_url) VALUES ($1, $2, $3, $4)
         RETURNING
         id AS "userId",
         full_name AS "fullName",
         email,
         profile_pic_url AS "profilePicURL",
         created_at AS "createdAt"
-       `,
-       [fullName, email, password, profilePicUrl]
-    );
+    `;
+    
+    const result = await pool.query(query, [fullName, email, password, profilePicUrl]);
 
     return result.rows[0];
 };
 
 export const findUserByEmail = async (email) => {
-    const result = await pool.query(
-        `SELECT
+    const query = `SELECT
         id AS "userId",
         full_name AS "fullName",
         email,
         password,
         profile_pic_url AS "profilePicURL",
         created_at AS "createdAt"
-        FROM users WHERE email = $1`, [email]
-    );
+        FROM users WHERE email = $1
+    `;
+    
+    const result = await pool.query(query, [email]);
 
     return result.rows[0];
 }
 
 export const findUserById  = async (id) => {
-    const result = await pool.query(
-        `SELECT
+    const query = `SELECT
         id AS "userId",
         full_name AS "fullName",
         email,
         profile_pic_url AS "profilePicURL",
         created_at AS "createdAt"
-        FROM users WHERE id = $1 `, [id]
-    );
+        FROM users WHERE id = $1
+    `;
+    
+    const result = await pool.query(query, [id]);
 
     return result.rows[0];
 }
