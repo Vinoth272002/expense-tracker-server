133f41bb35e23f99428d789f672957a3331a0431
vinoth <vinoth27172002@gmail.com>
2026-02-13 22:39:10 +0530

    refactor(incomeRoutes): simplify import statements for incomeController

--- src/routes/incomeRoutes.js
+++ src/routes/incomeRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { addIncome, getAllIncome, deleteIncome, downloadIncomeExcelFormat, getIncomeById, updateIncome } from '../controllers/incomeController.js';
+import * as incomeController from '../controllers/incomeController.js';
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, addIncome);
-router.get("/", protect, getAllIncome);
-router.get("/:incomeId", protect, getIncomeById);
-router.patch("/:incomeId", protect, updateIncome)
-router.get("/export/excel", protect, downloadIncomeExcelFormat);
-router.delete("/:incomeId", protect, deleteIncome);
+router.post("/", protect, incomeController.addIncome);
+router.get("/", protect, incomeController.getAllIncome);
+router.get("/:incomeId", protect, incomeController.getIncomeById);
+router.patch("/:incomeId", protect, incomeController.updateIncome)
+router.get("/export/excel", protect, incomeController.downloadIncomeExcelFormat);
+router.delete("/:incomeId", protect, incomeController.deleteIncome);
 
 export default router;
